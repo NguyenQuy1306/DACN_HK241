38943bc1ce99c468aa0e31aff4f2aee326c44a0f
nguyen quy <nngocquy75@gmail.com>
2025-05-18 19:47:02 +0700

    fix: replace CSV loading with API call to fetch restaurant data dynamically

--- BE/AI_Service/app/models/recommend_restaurant/predict.py
+++ BE/AI_Service/app/models/recommend_restaurant/predict.py
@@ -1,7 +1,20 @@
 import pickle
 import pandas as pd
+import requests
+  # Đặt file CSV ở gốc hoặc config sau
 
-restaurants_df = pd.read_csv('data/restaurants.csv')  # Đặt file CSV ở gốc hoặc config sau
+def get_restaurants_df():
+    url = "https://themeal.online/api/restaurants/all"  # Thay bằng URL thật
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return pd.DataFrame(data["payload"])
+    else:
+        raise Exception(f"Failed to fetch restaurants. Status code: {response.status_code}")
+    
+restaurants_df = get_restaurants_df()
+
 
 def load_model():
     with open("app/models/recommend_restaurant/model.pkl", "rb") as f:
@@ -14,7 +27,7 @@ def predict_top_n(user_features, top_n=5):
     for _, row in restaurants_df.iterrows():
         prob = model.predict_proba_one(user_features)
         score = prob.get(True, 0.0)
-        results.append((row["ID"], score))
+        results.append((row["maSoNhaHang"], score))
 
     # Sắp xếp và lấy top N
     sorted_results = sorted(results, key=lambda x: x[1], reverse=True)
